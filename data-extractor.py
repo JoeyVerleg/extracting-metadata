d316520fe12cae4db3c1411cdd3471625cdc7340
Joey Verleg <joey.verleg@student.uhasselt.be>
2019-03-13 14:20:22 +0100

    create DNS dictionary of all the DNS responses

--- data-extractor.py
+++ data-extractor.py
@@ -5,14 +5,33 @@ from scapy.layers.tls.all import *
 from collections import defaultdict
 
 CAPTURE_FILE_PATH = "/home/joey/Desktop/extracting-metadata/capture_tls.pcap"
+CAPTURE_FILTER = "host 2.18.169.16"
+load_layer("tls")
 
-def get_dns_packets():
-    packets = sniff(lfilter = lambda x: x.haslayer(DNS), offline=CAPTURE_FILE_PATH)
+def get_dns_responses():
+    packets = sniff(lfilter = lambda x: x.haslayer(DNSRR), offline=CAPTURE_FILE_PATH)
     return packets
 
 def get_tls_packets():
     packets = sniff(lfilter = lambda x: x.haslayer(TLS), offline=CAPTURE_FILE_PATH)
     return packets
+
+def get_tcp_packets():
+    packets = sniff(lfilter = lambda x: x.haslayer(TCP), filter=CAPTURE_FILTER, offline=CAPTURE_FILE_PATH)
+    return packets
+
+def get_all_packets():
+    packets = sniff(filter=CAPTURE_FILTER, offline=CAPTURE_FILE_PATH)
+    return packets
+
+def get_packet_size(packet, protocol):
+    # TODO: sometimes there are multiple TLS records into 'one' TLS record
+    if protocol == TLS:
+        layer = packet.getlayer(TLS)
+        return layer.len
+    elif protocol == TCP:
+        layer = packet.getlayer(TCP)
+        return len(layer)
 
 # Match DNS requests with their responses
 def match_dns_responses(packets):
@@ -37,11 +56,26 @@ def get_client_hellos():
             return
             serverName = p.getlayer(TLS_Ext_ServerName).servernames
             # print(serverName)
-                        
-dns_packets = get_dns_packets()
-match_dns_responses(dns_packets)
-get_client_hellos()
 
+# Create a DNS dictionary mapping all IP adresses to domains
+# example: dict['172.217.19.196'] = 'www.google.com' 
+def create_dns_dictionary():
+    responses = get_dns_responses()
+    dns_dict = dict()
+    for response in responses:
+        for x in range(response[DNS].ancount): # answer count, how many IP adresses are returned for the query
+            domain = getattr(response[DNSRR][x], 'rrname').decode("utf-8") # domain (this is returned in bytes so decode)
+            ip = getattr(response[DNSRR][x], 'rdata') # IP adres of the domain
+            dns_dict[ip] = domain
+    return dns_dict
+
+
+dns_dict = create_dns_dictionary()
+print(dns_dict.items())
+
+
+# print(get_packet_size(tls_packets[13], TLS))
+# print(get_packet_size(tls_packets[13], TCP))
 #wireshark client hello filter:
 #ssl.handshake.extensions_server_name
 
