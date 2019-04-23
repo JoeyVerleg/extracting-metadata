cc49c724c76780fa3d537b7bf98556b996eddd3b
Joey Verleg <joey.verleg@student.uhasselt.be>
2019-04-23 14:25:13 +0200

    update run attack script for better overview of output folder

--- run_attacks.sh
+++ run_attacks.sh
@@ -1,18 +1,18 @@
 #!/bin/bash  
 TRACES=$1
 OUTPUT=$2
-python extract_features.py --traces $TRACES --attack knn --out ../data/features/knn/
-python extract_features.py --traces $TRACES --attack kfp --out ../data/features/kfp/
-python extract_features.py --traces $TRACES --attack vng++ --out ../data/features/vng/
-python extract_features.py --traces $TRACES --attack ll --out ../data/features/ll/
-python extract_features.py --traces $TRACES --attack wright --out ../data/features/wright/
-python extract_features.py --traces $TRACES --attack CUMUL --out ../data/features/CUMUL/
-python extract_features.py --traces $TRACES --attack panchenko --out ../data/features/panchenko/
+python extract_features.py --traces $TRACES --attack knn --out ../$OUTPUT/features/knn/
+python extract_features.py --traces $TRACES --attack kfp --out ../$OUTPUT/features/kfp/
+python extract_features.py --traces $TRACES --attack vng++ --out ../$OUTPUT/features/vng/
+python extract_features.py --traces $TRACES --attack ll --out ../$OUTPUT/features/ll/
+python extract_features.py --traces $TRACES --attack wright --out ../$OUTPUT/features/wright/
+python extract_features.py --traces $TRACES --attack CUMUL --out ../$OUTPUT/features/CUMUL/
+python extract_features.py --traces $TRACES --attack panchenko --out ../$OUTPUT/features/panchenko/
 
-python classify.py --features ../data/features/knn/ --train 0.8 --test 0.2 --attack knn --out $OUTPUT_knn
-python classify.py --features ../data/features/kfp/ --train 0.8 --test 0.2 --attack knn --out $OUTPUT_kfp
-python classify.py --features ../data/features/vng/ --train 0.8 --test 0.2 --attack knn --out $OUTPUT_vng
-python classify.py --features ../data/features/ll/ --train 0.8 --test 0.2 --attack knn --out $OUTPUT_ll
-python classify.py --features ../data/features/wright/ --train 0.8 --test 0.2 --attack knn --out $OUTPUT_wright
-python classify.py --features ../data/features/CUMUL/ --train 0.8 --test 0.2 --attack knn --out $OUTPUT_CUMUL
-python classify.py --features ../data/features/panchenko/ --train 0.8 --test 0.2 --attack knn --out $OUTPUT_panchenko
+python classify.py --features ../$OUTPUT/features/knn/ --train 0.8 --test 0.2 --attack knn --out ../$OUTPUT/knn-result
+python classify.py --features ../$OUTPUT/features/kfp/ --train 0.8 --test 0.2 --attack kfp --out ../$OUTPUT/kfp-result
+python classify.py --features ../$OUTPUT/features/vng/ --train 0.8 --test 0.2 --attack vng++ --out ../$OUTPUT/vng-result
+python classify.py --features ../$OUTPUT/features/ll/ --train 0.8 --test 0.2 --attack ll --out ../$OUTPUT/ll-result
+python classify.py --features ../$OUTPUT/features/wright/ --train 0.8 --test 0.2 --attack wright --out ../$OUTPUT/wright-result
+python classify.py --features ../$OUTPUT/features/CUMUL/ --train 0.8 --test 0.2 --attack CUMUL --out ../$OUTPUT/CUMUL-result
+python classify.py --features ../$OUTPUT/features/panchenko/ --train 0.8 --test 0.2 --attack panchenko --out ../$OUTPUT/panchenko-result
