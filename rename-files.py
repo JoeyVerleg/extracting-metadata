2346a6cf2317b36494c88638c3fabea90806ea25
Joey Verleg <joey.verleg@student.uhasselt.be>
2019-05-08 17:11:17 +0200

    add start value parameter

--- rename-files.py
+++ rename-files.py
@@ -6,15 +6,18 @@ parser = argparse.ArgumentParser(description='Can be used for renaming features 
 parser.add_argument('dir', type=str, help='path to directory of files') 
 parser.add_argument('outputdir', type=str, help='path to output directory of renamed files') 
 parser.add_argument('amount', type=int, help='amount of files to copy and rename') 
+parser.add_argument('startvalue', type=int, help='start value of file name indices') 
 
 args = parser.parse_args()
 
 DIR_PATH = args.dir
 OUTPUT_DIR_PATH = args.outputdir
 AMOUNT = args.amount
+STARTVALUE = args.startvalue
+MAXPAGES = 100
 
 directory = os.fsencode(DIR_PATH)
-counter = [0] * AMOUNT
+counter = [0] * MAXPAGES
 if not os.path.isdir(OUTPUT_DIR_PATH):
     os.makedirs(OUTPUT_DIR_PATH)
 
@@ -26,8 +29,9 @@ for file in os.listdir(directory):
     new_value = counter[int(index)]
     if new_value >= AMOUNT:
         continue
-    new_name = str(index) + '-' + str(new_value)
+    new_name = str(index) + '-' + str(new_value + STARTVALUE)
     output_file_path = os.path.join(OUTPUT_DIR_PATH, new_name)
     shutil.copy(file_path, output_file_path)
+    print(file_path)
     print(output_file_path)
     counter[int(index)] += 1
